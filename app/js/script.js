2391ddef09bd65518dd17d69f660e99110033229
RocTanweer <mohammadtanweer769@gmail.com>
2021-06-10 10:24:56 +0530

    refactored the code and added event handler to select tag

--- app/js/script.js
+++ app/js/script.js
@@ -1,40 +1,63 @@
 const form = document.querySelector('.app__main-form');
-let coords;
-let mymap;
+const typeOfWorkouts = document.getElementById('typeOfWorkouts');
+const formElevation = document.querySelector('.app__main-form-elevation');
+const formCadence = document.querySelector('.app__main-form-cadence');
 
-// Using Geolocation API for the first time--------------
-const loc = navigator.geolocation.getCurrentPosition((position) => {
-    // getting the current position of the user in terms of latitute and longitude
-    const {latitude:lat, longitude:lon} = position.coords;
-    mymap = L.map('mapContainer').setView([lat, lon], 15);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(mymap);
-
-    mymap.on('click', (mapEvent) => {
-        const {lat, lng} = mapEvent.latlng;
-        coords = [lat, lng];
-        form.classList.remove('hidden');
-    })
-
-
-}, (error) => {
-    alert(`This is ${error}`)
-});
-// https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-// https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png
-
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+class App {
+    #coords;
+    #mymap;
     
-    L.marker(coords)
-        .addTo(mymap)
-        .bindPopup('workout', {
-            autoClose: false,
-            closeOnClick: false,
-            className: 'popup-cycling',
-        })
-        .openPopup();
-})
+    constructor() {
+        this._getCurrentLocation();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        typeOfWorkouts.addEventListener('change', this._toggleElevationField.bind(this));
+    }
+
+    _getCurrentLocation() {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), (error) => {
+                alert(`This is ${error}`)
+            })
+        }
+    }
+
+    _loadMap(position) {
+          // getting the current position of the user in terms of latitute and longitude
+            const { latitude: lat, longitude: lon } = position.coords;
+            this.mymap = L.map('mapContainer').setView([lat, lon], 15);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(this.mymap);
+
+            this.mymap.on('click', this._showForm.bind(this))
+    }
+
+    _showForm(mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        this.coords = [lat, lng];
+        form.classList.remove('hidden');
+    }
+
+    _toggleElevationField(e) {
+        formElevation.classList.toggle('hidden');
+        formCadence.classList.toggle('hidden');
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+
+        L.marker(this.coords)
+            .addTo(this.mymap)
+            .bindPopup('workout', {
+                autoClose: false,
+                closeOnClick: false,
+                className: 'popup-cycling',
+            })
+            .openPopup();
+    }
+
+
+}
+
+const app = new App();
