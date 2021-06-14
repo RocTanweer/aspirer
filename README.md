ae1a3c6671907936461591eeafb976dc57b38d65
RocTanweer <mohammadtanweer769@gmail.com>
2021-06-14 23:04:35 +0530

    commented script.js file and made the README.md file

---
+++ README.md
@@ -0,0 +1,32 @@
+# Aspirer
+A small Web Application which is made to help you manage your *running* and *cycling* activities. This is just another version of [mapty](https://mapty.netlify.app/), 
+a project made originally by [jonas](https://twitter.com/jonasschmedtman). I though why not making this all by myself from scratch and not just the JavaScript part that
+Jonas showed in his [udemyCoarse](https://www.udemy.com/course/the-complete-javascript-course/). So here is my version with some extra features.
+
+## Fully Responsive
+Aspirer is *fully responsive* for any screen width!
+
+## Languages, Techniques and Tools Used
+- HTML5
+- Sass(SCSS syntax)
+- Vanilla JavaScript
+- Leaflet
+- Fluid Typography
+- fontAwesome
+- Live server
+- Live-Sass compiler
+- Google Fonts
+- Adobe Spark
+- Vs Code 
+
+You may visit the site [here](https://roctanweer.github.io/aspirer/)
+
+## User's Can
+- Log their *running*/*cycling* activity with date, duration, distance, cadence/elevationGain, pace/speed with **location** on map
+- Put a *detailed* marker on the map by clicking on the map where the activity happened 
+- Move the map to corresponding marker when clicked on the logged activity
+- Delete a logged activity by clicking on the trash can icon along with the corresponding marker on the map
+- Delete all the logged activities along with all the markers on the map by clicking on the *Delete ALl* button
+- Revisit the application without loosing any of their data
+
+![site preview](./images/desktop-preview.jpg)
--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,47 @@
-const eventsContainer = document.querySelector('.app__main-events');
-const form = document.querySelector('.app__main-form');
-const formElevation = document.querySelector('.app__main-form-elevation');
-const formCadence = document.querySelector('.app__main-form-cadence');
-const typeOfWorkouts = document.getElementById('typeOfWorkouts');
-const inputDistance = document.getElementById('distance');
-const inputDuration = document.getElementById('duration');
-const inputCadence = document.getElementById('cadence');
-const inputElevation = document.getElementById('elevation');
-const deleteAllBtn = document.querySelector('.deleteAll');
+const eventsContainer = document.querySelector('.app__main-events');  // ul that contains all the workout events
+const form = document.querySelector('.app__main-form');  // form to create the workout events
+const formElevation = document.querySelector('.app__main-form-elevation');  // form section for elevation
+const formCadence = document.querySelector('.app__main-form-cadence');  // form section for cadence
+const typeOfWorkouts = document.getElementById('typeOfWorkouts');  // select tag for changing type of workout
+const inputDistance = document.getElementById('distance');  // distance field
+const inputDuration = document.getElementById('duration');  // duration field
+const inputCadence = document.getElementById('cadence');   // cadence field
+const inputElevation = document.getElementById('elevation');  // elevation field
+const deleteAllBtn = document.querySelector('.deleteAll');  // delete all button
 
+// Wherever you see 'common concept at the top', read the below documentation
+
+/*
+some common concept in this project-----
+
+# when an object is created then the constructor function is immediately called
+# we are initializing an object of class App in the global scope
+# All the codes in the constructor function of App gets executed right when the page loads
+
+$ keeping track of the value of 'this' in a class
+$ 'this' refers to the object who is calling it
+$ when calling 'addEventListener' on DOM element in class, 'this' refers to the respective DOM element
+$ but 'this' inside the callBack function must refer to the current obj of the class[as per the need]
+$ that is why, we are binding some callback function with 'this' which is the current obj[instance] of App
+
+% when an event[workout] is created, a marker is also created 
+% event is pushed in #workouts array and marker in #markers array both at the same index[simultaneously]
+% when an event is deleted then the corresponding marker must be deleted right away
+% So we are getting the index of event to be deleted and while deleting the event, we are also deleting the corresponding marker by retrieving it using that same index
+*/ 
+
+
+// to format the date 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+// parent class for common things between running and cycling
 class Workout {
-    constructor(distance, duration, coords) {
+    constructor(distance, duration, coords) {  // taking common arguments
         this.distance = distance;  //KM
         this.duration = duration;  //MIN
-        this.coords = coords;  // []
-        this.id = Date.now();
-        this._getDate();
+        this.coords = coords;  // [lat, lng]
+        this.id = Date.now();   // date in seconds from 1970 something
+        this._getDate();  // common concept at the top 
     }
 
     _getDate() {
@@ -26,27 +50,29 @@ class Workout {
     }
 }
 
+// blue print for running obj
 class Running extends Workout{
     constructor(distance, duration, coords, cadence) {
-        super(distance, duration, coords);
-        this.cadence = cadence;
-        this.type = 'running';
-        this.calcPace();
+        super(distance, duration, coords);  // constructor of parent class is called
+        this.cadence = cadence;  // steps per min
+        this.type = 'running';  // type of object
+        this.calcPace();  // common concept at the top 
     }
 
     calcPace() {
         const calc = this.duration / this.distance;
-        this.pace = +calc.toFixed(1);
+        this.pace = +calc.toFixed(1);  // limit the decimal place to 1
     }
 }
 
+// blue print for cycling obj
 class Cycling extends Workout {
     constructor(distance, duration, coords, elevationGain) {
-        super(distance, duration, coords);
-        this.elevationGain = elevationGain;
-        this.type = 'cycling';
-        this.calcSpeed();
-    }
+        super(distance, duration, coords);   // constructor of parent class is called
+        this.elevationGain = elevationGain;  // meters
+        this.type = 'cycling';  // type of objects
+        this.calcSpeed();   // common concept at the top 
+    } 
 
     calcSpeed() {
         const calc = this.distance / (this.duration / 60);
@@ -54,72 +80,88 @@ class Cycling extends Workout {
     }
 }
 
+
+// The application's main functionality 
 class App {
+    // private class fields 
     #coords;
     #mymap;
     #workouts;
     #markers = [];
     
+    // this function will be called as the object of App is initialized 
     constructor() {
-        this._getCurrentLocation();
-        form.addEventListener('submit', this._newWorkout.bind(this));
-        typeOfWorkouts.addEventListener('change', this._toggleElevationField.bind(this));
-        // setTimeout(() => {
-        //     this._renderFromLS();
-        // }, 500);
+        this._getCurrentLocation(); 
+        form.addEventListener('submit', this._newWorkout.bind(this)); // common concept at the top 
+        typeOfWorkouts.addEventListener('change', this._toggleElevationField.bind(this));  // common concept at the top 
 
-        this._renderFromLS();
-        eventsContainer.addEventListener('click', this._moveToMarker.bind(this));
-        deleteAllBtn.addEventListener('click', this._deleteAllWorkouts.bind(this));
+        // setTimeout(() => {
+        //     this._renderFromLS();  // here the marker was loading right when the page laods, which is not possible because map is not rendered  
+        // }, 500);                  //  that is why i was waiting 500ms so the map gets rendered and then marker can be rendered
+
+        this._renderFromLS();  // calling this right away because the marker will render after the map is render and not when page loads[see the _loadMap method]
+        eventsContainer.addEventListener('click', this._moveToMarker.bind(this));  // common concept at the top 
+        deleteAllBtn.addEventListener('click', this._deleteAllWorkouts.bind(this));  // common concept at the top 
     }
 
+    // get called right when page loads
     _getCurrentLocation() {
         if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), (error) => {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), (error) => {  // common concept at the top
                 alert(`This is ${error}`)
             })
         }
     }
 
+    // gets called when we get the user's position
     _loadMap(position) {
           // getting the current position of the user in terms of latitute and longitude
             const { latitude: lat, longitude: lon } = position.coords;
-            this.#mymap = L.map('mapContainer').setView([lat, lon], 15);
+            this.#mymap = L.map('mapContainer').setView([lat, lon], 15);  // making the map at user's position
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {  // adding style of the map
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',  // attribution to leaflet
             }).addTo(this.#mymap);
 
-            this.#mymap.on('click', this._showForm.bind(this));
+            this.#mymap.on('click', this._showForm.bind(this));  // common concept at the top
 
+            // after the map is render, the marker is rendered
             this.#workouts.forEach(workout => {
                 this._renderMarker(workout);
             })
     }
 
+    // gets called when user clicks on map
     _showForm(mapEvent) {
+        // position where the user clicked on the map
         const { lat, lng } = mapEvent.latlng;
-        this.#coords = [lat, lng];
+        this.#coords = [lat, lng];  // storing this value in the private field so that it can be used in other method because let and const are inaccessible out of that block
         form.classList.remove('hidden');
         inputDistance.focus();
     }
 
+    // gets called when value of select tag is changed
     _toggleElevationField(e) {
         formElevation.classList.toggle('hidden');
         formCadence.classList.toggle('hidden');
     }
 
+    // utility method to check if input is a valid number and not +ve or -ve infinity
     _validNum(...nums) {
         return nums.every(num => Number.isFinite(num));
     }
 
+    // utility method to check if input is positive
     _arePositive(...nums) {
         return nums.every(num => num > 0)
     }
 
+    // gets called when the form is submit
     _renderMarker(workout) {
-        const marker = L.marker(workout.coords);
+        // making the marker/layer at the position where the user clicked by getting it from private field [see _showform method]
+        const marker = L.marker(workout.coords);  
         
+        // adding the marker in the map with some custom HTMl and CSS to the popup
         marker.addTo(this.#mymap)
             .bindPopup(`<p class="popup">${workout.type === 'running' ? '🏃‍♀️' : '🚴'} ${workout.type[0].toUpperCase()}${workout.type.slice(1)} on ${workout.date}</p>`,
                 {
@@ -129,10 +171,12 @@ class App {
                 })
             .openPopup();
 
-        this.#markers.push(marker)
+        this.#markers.push(marker)  // keeping all the markers/layers that the user is created in #markers array sequence-wise for later use
     }
 
+    // gets called when the form is submit
     _renderWorkout(workout) {
+        // using workout obj that is either created dynamically or comming from LS 
         let htmlContent = `
             <li class="app__main-events-event workout-${workout.type}" id=${workout.id}>
                 <div class="app__main-events-event-top">
@@ -154,7 +198,7 @@ class App {
                     <span class="app__main-events-event-bottom-activity">🕰️ ${workout.duration} <small>MIN</small></span>
         `;
 
-        if(workout.type === 'running') {
+        if(workout.type === 'running') {  // to make sure, different UI is created for different workout
             htmlContent += `
                         <span class="app__main-events-event-bottom-activity">🏃‍♀️ ${workout.pace} <small>MIN/KM</small></span>
                         <span class="app__main-events-event-bottom-activity">🦶 ${workout.cadence} <small>SPM</small></span>
@@ -164,7 +208,7 @@ class App {
             `;
         };
 
-        if(workout.type === 'cycling') {
+        if(workout.type === 'cycling') {  // to make sure, different UI is created for different workout
             htmlContent += `
                         <span class="app__main-events-event-bottom-activity">🚴 ${workout.speed} <small>KM/H</small></span>
                         <span class="app__main-events-event-bottom-activity">🗻 ${workout.elevationGain} <small>M</small></span>
@@ -176,82 +220,83 @@ class App {
         eventsContainer.insertAdjacentHTML('afterbegin', htmlContent);        
     }
 
+    // gets called when page loads[see constructor function]
     _renderFromLS() {
-        const datas = JSON.parse(localStorage.getItem('workouts')) || [];
-        this.#workouts = datas;
+        const datas = JSON.parse(localStorage.getItem('workouts')) || [];  
+        this.#workouts = datas;  // keeping the data coming from LS into #workouts array
 
         datas.forEach((data) => {
-            this._renderWorkout(data);
+            this._renderWorkout(data);  // rendering the events when data is coming from LS
         })
     }
 
+    // gets called when the form is submit
     _newWorkout(e) {
-        e.preventDefault();
-
+        e.preventDefault();  // to prevent default behaviour of form to submit data to a server
+        // inputs from the user's using that form
         const type = typeOfWorkouts.value;
         const distance = inputDistance.value;
         const duration = inputDuration.value;
-        let workout;
+        let workout;  // making it at local scope so that other blocks can use it together
 
-        if(type === 'running') {
+        if(type === 'running') {  // two child classes
             const cadence = inputCadence.value;
-            
+            // using those utility classes 
             if(!this._validNum(distance, duration, cadence) && !this._arePositive(distance, duration, cadence)) return
-
+            // creating Running obj as type is running
             workout = new Running(distance, duration, this.#coords, cadence);    
         }
 
-        if (type === 'cycling') {
+        if (type === 'cycling') {  // two child classes
             const elevation = inputElevation.value;
-
+            // using those utility classes 
             if (!this._validNum(distance, duration, elevation) && !this._arePositive(distance, duration)) return
-
+            // creating Cylick obj as type is cycling
             workout = new Cycling(distance, duration, this.#coords, elevation);
         }
-        
-        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-        // console.log(workout)
-
+        // input fields must be emptied after form submission
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';  
+        // form -> display:none;
         form.classList.add('hidden');
-
-
+        // updating the #workouts array after user created an obj
         this.#workouts.push(workout);
-        localStorage.setItem('workouts', JSON.stringify(this.#workouts));
-
+        localStorage.setItem('workouts', JSON.stringify(this.#workouts));  // updating LS too
         this._renderMarker(workout);
-
         this._renderWorkout(workout);
     }
 
+    // gets called when clicked on eventsContainer[see constructor function]
     _moveToMarker(e) {
-        const workoutFromUI = e.target.closest('.app__main-events-event');
-
-        if(!workoutFromUI) return
-        
+        const workoutFromUI = e.target.closest('.app__main-events-event');  // getting the event on which user clicked
+        if(!workoutFromUI) return 
+        // getting the corresponding obj from #workers array 
         const clickedWorkout = this.#workouts.find(workout => workout.id === +workoutFromUI.id);
+        // getting index of the corresponding obj from #workers array 
         const index = this.#workouts.findIndex(workout => workout.id === +workoutFromUI.id);
-
+        // moving the map to the corresponding marker 
         this.#mymap.setView(clickedWorkout.coords, 15, {
             animate: true,
         })
-
+        // common concept at the top
         const currentMarker = this.#markers[index];
 
         if(e.target.classList.contains('delete')) {
-            eventsContainer.removeChild(workoutFromUI);
-            this.#workouts.splice(index, 1);
-            localStorage.setItem('workouts', JSON.stringify(this.#workouts));
-            currentMarker.remove();
+            eventsContainer.removeChild(workoutFromUI);  // deleting from UI
+            this.#workouts.splice(index, 1);  // deleting from #workouts
+            localStorage.setItem('workouts', JSON.stringify(this.#workouts));  // updating the LS
+            currentMarker.remove();  // common concept at the top
         }
 
     }
 
+    // gets called when that delete button is clicked
     _deleteAllWorkouts(e) {
-        eventsContainer.textContent = '';
+        // deleting all the events from both UI and LS
+        eventsContainer.textContent = ''; 
         this.#workouts = [];
         localStorage.setItem('workouts', JSON.stringify(this.#workouts));
-        this.#markers.forEach(marker => marker.remove());
+        this.#markers.forEach(marker => marker.remove());  // common concept at the top
     }
 }
 
-const app = new App();
+const app = new App();  // constructor function will now be called right when page loads
